51ff305476c9bec6848a22c6eac0207890371482
golub001 <aleksandarg03@gmail.com>
2026-05-08 22:28:03 +0200

    dodao sam back

---
+++ back/Backend/Services/MongoDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Backend.Models;
+using Backend.Settings;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace Backend.Services
+{
+    public class MongoDbContext
+    {
+        private readonly IMongoDatabase _database;
+        
+        public MongoDbContext(IOptions<MongoDbSettings> settings)
+        {
+            var client = new MongoClient(settings.Value.ConnectionString);
+            _database=client.GetDatabase(settings.Value.DatabaseName);
+        }
+        public IMongoCollection<Category> Categories =>
+            _database.GetCollection<Category>("categories");
+
+        
+
+    }
+}
+
